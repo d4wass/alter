a1694dd3eeb674dd9465873849c61f36e5a6c004
d4wass <dwasilewski.93@gmail.com>
2021-12-05 19:28:08 +0100

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Alter';
+
   number = 'dalskjdaslkdmals';
+
+  showNumber(): void {
+    console.log(this.number);
+  }
 }
