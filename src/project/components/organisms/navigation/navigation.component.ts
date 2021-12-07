6c1f52911f9e8c8beb905a41f5398d1ff9dc90e8
d4wass <dwasilewski.93@gmail.com>
2021-12-07 20:15:53 +0100

    testing component

---
+++ src/project/components/organisms/navigation/navigation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-navigation',
+  template: '<p>home navigation</p>',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent {
+  constructor() {}
+}
