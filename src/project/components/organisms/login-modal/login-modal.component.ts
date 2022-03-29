b0de3eb74f98625342030c3083db28658563dd99
d4wass <dwasilewski.93@gmail.com>
2022-03-29 13:25:28 +0200

    creating change view for login modal

--- src/project/components/organisms/login-modal/login-modal.component.ts
+++ src/project/components/organisms/login-modal/login-modal.component.ts
@@ -1,5 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ModalLoginService } from 'src/project/services/modal-login-service.service';
 
 @Component({
@@ -7,22 +6,25 @@ import { ModalLoginService } from 'src/project/services/modal-login-service.serv
   template: `
     <ng-container *ngIf="isVisible">
       <div class="wrapper" appLoginModal>
-        <div class="wrapper-modal">
-          <h1 class="title">Login to your account</h1>
+        <div class="wrapper-modal" appLoginModal>
+          <h1 class="title" *ngIf="isLogin">Login to your account</h1>
+          <h1 class="title" *ngIf="!isLogin">Create an account</h1>
           <div class="content">
-            <div class="inputs">
-              <input type="text" placeholder="email" class="input" />
-              <input type="password" placeholder="password" class="input" />
-            </div>
-            <div class="buttons">
-              <button class="standard-btn">Confirm</button>
-              <button class="cancel-btn" (click)="onCancel()">cancel</button>
-            </div>
-            <div class="divider">
-              <span></span>
-              <p>Or</p>
-              <span></span>
-            </div>
+            <ng-container *ngIf="isLogin">
+              <div class="inputs">
+                <input type="text" placeholder="email" class="input" />
+                <input type="password" placeholder="password" class="input" />
+              </div>
+              <div class="buttons">
+                <button class="standard-btn">Confirm</button>
+                <button class="cancel-btn" (click)="onCancel()">cancel</button>
+              </div>
+              <div class="divider">
+                <span></span>
+                <p>Or</p>
+                <span></span>
+              </div>
+            </ng-container>
             <div class="buttons">
               <button class="social-btn">
                 <img src="assets/facebook.svg" />
@@ -36,7 +38,26 @@ import { ModalLoginService } from 'src/project/services/modal-login-service.serv
                 <img src="assets/apple.svg" />
                 <p>Continue with Apple</p>
               </button>
+              <button class="social-btn">
+                <img src="assets/envelope.svg" />
+                <p>Continue with Email</p>
+              </button>
             </div>
+            <div class="divider">
+              <span></span>
+            </div>
+            <ng-container *ngIf="!isLogin">
+              <div class="change-btn">
+                <p>Already have an account?</p>
+                <button (click)="setStateModalView($event)" id="login">Log in</button>
+              </div>
+            </ng-container>
+            <ng-container *ngIf="isLogin">
+              <div class="change-btn">
+                <p>Don't have an account?</p>
+                <button (click)="setStateModalView($event)" id="signup">Sign up</button>
+              </div>
+            </ng-container>
           </div>
         </div>
       </div>
@@ -44,21 +65,35 @@ import { ModalLoginService } from 'src/project/services/modal-login-service.serv
   `,
   styleUrls: ['./login-modal.component.scss']
 })
-export class LoginModalComponent implements OnInit {
+export class LoginModalComponent implements OnInit, OnDestroy {
   isVisible?: boolean;
+  isLogin?: boolean;
 
   constructor(private modalLoginService: ModalLoginService) {}
 
-  @HostListener('document:keyup.escape', ['$event'])
-  onEscape(): void {
-    this.modalLoginService.isVisible$.next(false);
+  ngOnInit() {
+    this.modalLoginService.isVisible$.subscribe((isVisible) => (this.isVisible = isVisible));
+    this.modalLoginService.isLogin$.subscribe((isLogin) => (this.isLogin = isLogin));
   }
 
-  ngOnInit() {
-    this.modalLoginService.isVisible$.subscribe((val) => (this.isVisible = val));
+  ngOnDestroy(): void {
+    this.modalLoginService.isVisible$.unsubscribe();
+    this.modalLoginService.isLogin$.unsubscribe();
   }
 
   onCancel() {
     this.modalLoginService.isVisible$.next(false);
+  }
+
+  setStateModalView(event: Event) {
+    let target = event.target as HTMLElement;
+
+    if (target.id === 'login') {
+      this.modalLoginService.isLogin$.next(true);
+    }
+
+    if (target.id === 'signup') {
+      this.modalLoginService.isLogin$.next(false);
+    }
   }
 }
