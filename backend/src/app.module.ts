ea7ccd788032148261dd59b40cd119f9adace352
d4wass <dwasilewski.93@gmail.com>
2022-05-25 23:29:56 +0200

    implement jwt

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -7,17 +7,20 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users/users.controller';
 import { VehiclesController } from './vehicles/vehicles.controller';
 import { VehiclesModule } from './vehicles/vehicles.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UsersModule,
     VehiclesModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://damian:mm6uoLF17ZprMlMp@cluster0.xnysy.mongodb.net/Alter?retryWrites=true&w=majority'
-    )
+    MongooseModule.forRoot(process.env.DATABASE_URI)
   ],
   controllers: [AppController, UsersController, VehiclesController],
   providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+  @Inject(ConfigService)
+  public config: ConfigService;
+}
