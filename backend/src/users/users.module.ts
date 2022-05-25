8148116cc5719256bee1154f72cae663f66412d2
d4wass <42357500+d4wass@users.noreply.github.com>
2022-05-25 21:31:55 +0200

    Security config (#29)
    * init backend
    
    * security changes

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from 'src/models/user.model';
+import { AuthService } from 'src/auth/auth.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
