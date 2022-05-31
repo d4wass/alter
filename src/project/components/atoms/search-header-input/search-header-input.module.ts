8a408ea5eda165ca3e2cc60b7a5b34e5f23ce990
d4wass <42357500+d4wass@users.noreply.github.com>
2022-05-31 23:03:08 +0200

    Backend frontend search mechanism (#31)
    * init backend
    
    * setting backend search api to frontend

---
+++ src/project/components/atoms/search-header-input/search-header-input.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchHeaderInputComponent } from './search-header-input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [SearchHeaderInputComponent],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [SearchHeaderInputComponent]
+})
+export class SearchHeaderInputModule {}
