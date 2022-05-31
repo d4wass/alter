2cf90baee46e2ce4d0b1266958dbb3d0d59fc068
d4wass <dwasilewski.93@gmail.com>
2022-05-31 23:01:45 +0200

    setting backend search api to frontend

--- backend/src/vehicles/vehicles.service.ts
+++ backend/src/vehicles/vehicles.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Vehicle } from '../models/vehicle.model';
 import { Model } from 'mongoose';
@@ -24,8 +24,60 @@ export class VehiclesService {
   //search request
   // async getAllVehicles() {}
   // async getVehiclesByModel() {}
-  // async getVehiclesByBrand() {}
-  // async getVehiclesByPlace() {}
+  async getVehiclesByBrand(brand: string) {
+    let vehiclesByBrand: Vehicle[];
+    try {
+      vehiclesByBrand = await this.vehicleModel.find({ brand }).exec();
+    } catch (error) {
+      throw new NotFoundException('Cannot find vehicles by brand');
+    }
+
+    if (!vehiclesByBrand) {
+      throw new NotFoundException('Cannot find vehicles by brand');
+    }
+
+    return vehiclesByBrand as Vehicle[];
+  }
+
+  async getVehiclesByQuery(query: any) {
+    let vehiclesByQuery: Vehicle[];
+    try {
+      vehiclesByQuery = await this.vehicleModel.find(query).exec();
+    } catch (error) {
+      throw new NotFoundException('Cannot find vehicles by brand');
+    }
+
+    if (!vehiclesByQuery) {
+      throw new NotFoundException('Cannot find vehicles by brand');
+    }
+
+    return vehiclesByQuery as Vehicle[];
+  }
+
+  async getVehiclesByPlace(place: string) {
+    let vehiclesByPlace: Vehicle[];
+    try {
+      vehiclesByPlace = await this.vehicleModel.find({ place }).exec();
+    } catch (error) {
+      throw new NotFoundException('Cannot find vehicles by place');
+    }
+
+    if (!vehiclesByPlace) {
+      throw new NotFoundException('Cannot find vehicles by place');
+    }
+
+    return vehiclesByPlace as Vehicle[];
+  }
   // async getVehiclesByHost() {}
   // async getVehicle() {}
+  async getAllVehicles() {
+    const vehicles = await this.vehicleModel.find().exec();
+    console.log(vehicles);
+    return vehicles;
+  }
+
+  stringFormatter(value: string): string {
+    const formattedParam = value.charAt(0).toUpperCase() + value.toLowerCase().slice(1);
+    return formattedParam;
+  }
 }
