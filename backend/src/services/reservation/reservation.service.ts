e940be1d20c1e2a2f05fd331bc26c253cecbfb93
d4wass <42357500+d4wass@users.noreply.github.com>
2023-08-29 11:48:13 +0200

    implementing prevention before creating reservation if one of relateddocument not exists (#67)

--- backend/src/services/reservation/reservation.service.ts
+++ backend/src/services/reservation/reservation.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { ICrud } from 'interface/crud.interface';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { ReservationDate, ReservationModel } from 'src/models/reservations/reservation.model';
 import { Reservation, ReservationDocument } from 'src/schemas/reservation/reservation.schema';
 import * as moment from 'moment';
@@ -21,30 +21,44 @@ export class ReservationService implements ICrud<Reservation, ReservationDto, st
     let reservation;
     const { hostId, userId, vehicleId, price, endDate, fromDate } = reservationData;
     const totalPrice = this.calculateRentalCost(fromDate, endDate, price);
+    const host = await this.userModel.findById(hostId).exec();
+    const user = await this.userModel.findById(userId).exec();
+    const vehicle = await this.vehicleModel.findById(vehicleId).exec();
+    const isHostVehicle = await this.vehicleModel.findOne({ owner: hostId }).exec();
+    console.log(isHostVehicle);
 
-    try {
-      if (reservationData.hostId !== reservationData.userId) {
-        const newReservation = new this.reservationModel({
-          host: hostId,
-          user: userId,
-          vehicle: vehicleId,
-          cost: totalPrice,
-          ...reservationData
-        });
-        reservation = await newReservation.save();
-      } else {
-        //TODO: consider to create custom exception for case when user tries to rent car from itself
-        throw new HttpException('Cannot make a reservation from itself', HttpStatus.BAD_REQUEST);
-      }
+    if (!host || !user || !vehicle) {
+      const documentNotExists = {
+        host: !!host,
+        user: !!user,
+        vehicle: !!vehicle
+      };
+      const msg = this.getErrorMsg(documentNotExists);
+      throw new HttpException(`Cannot create reservation, ${msg}`, HttpStatus.NOT_FOUND);
+    }
 
-      if (reservation) {
-        this.usersReservationUpdate(hostId, userId, reservation._id);
-        await this.vehicleModel
-          .findByIdAndUpdate({ _id: vehicleId }, { $push: { avalibility: reservation._id } })
-          .exec();
-      }
-    } catch (error) {
-      throw new HttpException('Cannot create reservation', HttpStatus.BAD_REQUEST);
+    if (reservationData.hostId !== reservationData.userId && !!host && !!user && !!vehicle) {
+      const newReservation = new this.reservationModel({
+        host: hostId,
+        user: userId,
+        vehicle: vehicleId,
+        cost: totalPrice,
+        ...reservationData
+      });
+      reservation = await newReservation.save();
+    }
+
+    // if (!) {
+
+    // }
+
+    if (!!reservation) {
+      this.usersCreateReservationUpdate(hostId, userId, reservation._id);
+      await this.vehicleModel
+        .findByIdAndUpdate({ _id: vehicleId }, { $push: { avalibility: reservation._id } })
+        .exec();
+    } else {
+      throw new HttpException('Cannot create reservation', HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     return { reservationId: reservation._id };
@@ -65,8 +79,15 @@ export class ReservationService implements ICrud<Reservation, ReservationDto, st
   }
 
   async delete(reservationId: string): Promise<void> {
+    const reservation = await this.reservationModel.findById(reservationId).exec();
+    const { user, host, vehicle } = reservation;
+
     try {
       await this.reservationModel.findByIdAndDelete(reservationId);
+      this.usersDeleteReservationUpdate(host, user, reservationId);
+      await this.vehicleModel.findByIdAndUpdate(vehicle, {
+        $pull: { avalibility: new Types.ObjectId(reservationId) }
+      });
     } catch (error) {
       throw new NotFoundException('Cannot delete reservation');
     }
@@ -86,9 +107,33 @@ export class ReservationService implements ICrud<Reservation, ReservationDto, st
     throw new Error('Method not implemented.');
   }
 
-  private async usersReservationUpdate(hostId: string, userId: string, reservationId: string) {
+  private async usersCreateReservationUpdate(
+    hostId: string,
+    userId: string,
+    reservationId: string
+  ) {
     await this.userModel.findByIdAndUpdate(hostId, { $push: { reservations: reservationId } });
     await this.userModel.findByIdAndUpdate(userId, { $push: { reservations: reservationId } });
+  }
+
+  private getErrorMsg<T extends Record<string, boolean>>(obj: T): string[] {
+    const failedDocument: string[] = [];
+    for (const key in obj) {
+      if (!obj[key]) {
+        failedDocument.push(`${key} was not found`);
+      }
+    }
+    return failedDocument;
+  }
+
+  private async usersDeleteReservationUpdate(
+    hostId: string,
+    userId: string,
+    reservationId: string
+  ) {
+    const id = new Types.ObjectId(reservationId);
+    await this.userModel.findByIdAndUpdate(hostId, { $pull: { reservations: id } });
+    await this.userModel.findByIdAndUpdate(userId, { $pull: { reservations: id } });
   }
 
   private calculateRentalCost(fromDate: ReservationDate, endDate: ReservationDate, price: number) {
