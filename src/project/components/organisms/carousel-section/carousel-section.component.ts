00e445217a79ff60546f0d5241b8726d5fcf99c8
d4wass <dwasilewski.93@gmail.com>
2024-02-11 19:30:40 +0100

    work on carousel logic pt.2

--- src/project/components/organisms/carousel-section/carousel-section.component.ts
+++ src/project/components/organisms/carousel-section/carousel-section.component.ts
@@ -6,18 +6,17 @@ import {
   Component,
   ElementRef,
   QueryList,
-  Renderer2,
   ViewChild,
   ViewChildren
 } from '@angular/core';
-import { iif } from 'rxjs';
-import { CarCardComponent } from '../../molecules/car-card/car-card.component';
 
 const carCards = [
   { title: 'BMW', img: 'assets/car.png' },
   { title: 'Toyota', img: 'assets/car.png' },
   { title: 'Mazda', img: 'assets/car.png' },
   { title: 'Jeep', img: 'assets/car.png' },
+  { title: 'Mercedes-Benz', img: 'assets/car.png' },
+  { title: 'Mercedes-Benz', img: 'assets/car.png' },
   { title: 'Mercedes-Benz', img: 'assets/car.png' },
   { title: 'Mercedes-Benz', img: 'assets/car.png' },
   { title: 'Mercedes-Benz', img: 'assets/car.png' },
@@ -56,7 +55,8 @@ export class CarouselSectionComponent implements AfterViewInit {
   private player!: AnimationPlayer;
   private carouselItems!: HTMLElement[];
   private carouselWidth!: number;
-  offset: number = 0;
+  private counter = 0;
+  private offset: number = 0;
 
   constructor(private animationBuilder: AnimationBuilder, private cdr: ChangeDetectorRef) {}
   //todo: whats the difference between viewChild and ViewChildren
@@ -93,14 +93,45 @@ export class CarouselSectionComponent implements AfterViewInit {
     ]);
   }
 
+  private counterDirectionControl(direction: string, maxCounterValue: number): void {
+    let counter = this.counter;
+    if (direction === '' && this.counter !== 0) {
+      --counter;
+      this.counter = counter;
+    }
+
+    if (direction === '-' && this.counter === maxCounterValue) {
+      this.counter = counter;
+    } else if (direction === '-') {
+      ++counter;
+      this.counter = counter;
+    }
+  }
+
   private checkCurrentOffset(direction: string, itemWidth: number, maxOffset: number): number {
     let newOffset = this.offset;
+    const num = this.carouselWidth / this.carouselItems[0].clientWidth;
+    const roundedNum = this.roundNumber(num); // how many entire cards I can display for user
+    const numerOfClicksToDisplayLastCard = this.carouselItems.length - Math.floor(roundedNum) - 1;
+    //! watch for negative in case for roundedNum !
+    const movePx = itemWidth * this.roundNumber(roundedNum - Math.floor(roundedNum)) + 25 + 20;
+    // console.log(roundedNum);
+    // console.log(movePx);
+    // console.log(this.counter, 'lastClick', numerOfClicksToDisplayLastCard);
+    this.counterDirectionControl(direction, numerOfClicksToDisplayLastCard);
+    console.log(this.counter);
 
     if (direction === '' && newOffset === 0) return (newOffset = 0);
     if (direction === '-' && newOffset === 0) return (newOffset = newOffset - itemWidth);
-    if (direction === '-' && newOffset <= -maxOffset) return (newOffset = -maxOffset);
+    if (direction === '-' && newOffset !== 0 && this.counter === numerOfClicksToDisplayLastCard)
+      return (newOffset = newOffset - movePx);
+    if (direction === '-' && newOffset <= -maxOffset) return (newOffset = -maxOffset - 29);
     if (direction === '' && newOffset > 0) return (newOffset = 0);
 
     return direction ? newOffset - itemWidth : newOffset + itemWidth;
+  }
+
+  private roundNumber(num: number): number {
+    return Math.round((num + Number.EPSILON) * 100) / 100;
   }
 }
